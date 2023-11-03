4c20f1d3b554fecfe6953b23e68c10f2da409809
samjayworldwide <samuel.mbanisi@decagon.dev>
2023-11-03 19:43:47 +0100

    security commit

---
+++ src/main/java/com/example/securityTutorial/SecurityTutorialApplication.java
@@ -0,0 +1,19 @@
+package com.example.securityTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityTutorialApplication {
+	//	echo "# teacher-cicd" >> README.md
+//	git init
+//	git add README.md
+//	git commit -m "first commit"
+//	git branch -M main
+//	git remote add origin https://github.com/Samjayworldwide/teacher-cicd.git
+//	git push -u origin main
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityTutorialApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/securityTutorial/user/Role.java
@@ -0,0 +1,6 @@
+package com.example.securityTutorial.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
